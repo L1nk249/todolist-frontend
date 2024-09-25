ce54b7dc9b98b1779149e7201742a26ca715c67e
Cédric LEVY <levycedric@hotmail.fr>
2024-09-25 12:45:12 +0200

    todook

--- app/page.js
+++ app/page.js
@@ -1,4 +1,55 @@
+
+'use client'
+import {Box,TextField,Button,Grid} from '@mui/material';
+import { useState } from 'react';
+
+
 export default function Home(){
- return <h1>Todo</h1>;
+    const [todos,setTodos]=useState([])
+    const [todo,setTodo]=useState('')
+
+const handleAdd=()=>{
+    setTodos((prev)=>[... prev,todo])
+    setTodo ("")
+}
+const handleDelete=(index)=>{
+setTodos((prev)=>prev.filter((_,i)=>i!==index))
 }
 
+
+ return  (  <>
+ <Box>
+ <Grid container spacing={2}>
+ <Grid item xs={6}>
+
+   <TextField id="outlined-basic" label="TODO" variant="outlined"
+   fullWidth
+   value={todo}
+   onChange={(e)=>setTodo(e.target.value)}/>
+
+   </Grid>
+   <Grid item xs={6}
+   >
+
+<Button variant="contained"
+fullWidth
+sx={{height:'100%',}}
+onClick={handleAdd}
+
+>Ajouter un ToDo</Button>
+</Grid>
+<Grid item xs={12}>
+<ul>
+   {todos.map((todo,index)=>(<li key={index}>{todo}
+   < Button onClick={()=>handleDelete(index)}
+   >Supprimer
+  </Button>
+   </li>))} 
+</ul>
+
+</Grid>
+ </Grid>
+ </Box>
+</>
+);
+}
