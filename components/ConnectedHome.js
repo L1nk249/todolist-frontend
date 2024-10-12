b682cb95ee10db8ed3b86fcad324d06a6f62adf2
Cédric LEVY <levycedric@hotmail.fr>
2024-10-12 18:56:34 +0200

    pictureok

--- components/ConnectedHome.js
+++ components/ConnectedHome.js
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography,Grid } from "@mui/material";
 import { useState } from "react";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,17 +11,19 @@ const Section = ({ imageSrc, altText, title }) => (
       display: "flex",
       alignItems: "center",
       flexDirection: "column",
-      justifyContent: "space-between",
+      justifyContent: "align-items",
       color: "#333", // Couleur du texte
       padding: "10px 20px",
       borderRadius: "25px", // Bordure arrondie pour effet capsule
       fontSize: "1.5rem",
       boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)", // Légère ombre pour un effet de relief
-      minWidth: "800px", // Largeur minimum pour la capsule
-      maxWidth: "800px", // Largeur maximum
+      minWidth: "500px", // Largeur minimum pour la capsule
+      maxWidth: "500px", // Largeur maximum
       width: "100%", // Ajuste la largeur au contenu
       textAlign: "center",
-      marginBottom: "20px", // Espace entre les sections
+      marginTop:"10px",
+      marginBottom: "2px", // Espace entre les sections
+      backgroundColor:"white"
     }}
   >
     <Box
@@ -39,8 +41,8 @@ const Section = ({ imageSrc, altText, title }) => (
     <Typography
       variant="h5"
       sx={{
-        fontSize: "1.5rem", // Taille du titre
-        color: "#333", // Couleur du titre
+        fontSize: "2.5rem", // Taille du titre
+        color: "#996868", // Couleur du titre
         fontWeight: "bold", // Style gras pour le titre
       }}
     >
@@ -54,25 +56,52 @@ export default function ConnectedHome() {
     <>
       <div
         style={{
+          marginTop:"100px",
+          marginBottom: "50px", // Marge pour éviter de chevaucher le footer
           height: "100vh",
+          minHeight: "calc(100vh - 120px)", // Hauteur totale moins la taille du header et du footer
           position: "fixed",
           top: 0,
           left: 0,
           backgroundImage: "url(/backgroundBisConnected.jpeg)", // chemin relatif à l'image
+          opacity:0.8,
           backgroundSize: "cover", // couvre toute la zone
           backgroundAttachment: "fixed",
           backgroundPosition: "center", // centre l'image
           minHeight: "100vh",
           width: "100%",
           zIndex: -1, // met l'image en arrière-plan
-        }}
-      >
-        {/* Utilisation du composant réutilisable Section pour chaque contenu */}
-        <Section imageSrc="/bricolage.jpeg" altText="Bricolage" title="Bricolage" />
-        <Section imageSrc="/ecole.jpeg" altText="Ecole" title="Ecole" />
-        <Section imageSrc="/sport.jpeg" altText="Sport" title="Sport" />
-        <Section imageSrc="/courses.jpeg" altText="Liste de courses" title="Liste de courses" />
-        <Section imageSrc="/historique.jpeg" altText="Todos sauvegardés" title="Todos sauvegardés" />
+        }}>
+
+<Grid
+          container
+          spacing={8}
+          justifyContent="center"
+          alignItems="flex-start" // Aligner en haut pour éviter l'espace vertical
+          sx={{
+          
+            margin: "0 auto", // Centre la section
+            padding: "20px 20px",  // Ajoute de l'espace autour du contenu
+
+          }}
+        >
+          {/* Chaque élément occupe 6 colonnes sur un total de 12 (soit deux éléments par ligne) */}
+          <Grid item xs={12} sm={6} md={4}>
+            <Section imageSrc="/bricolage.jpeg" altText="Bricolage" title="Bricolage" />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Section imageSrc="/ecole.jpeg" altText="Ecole" title="Ecole" />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Section imageSrc="/sport.jpeg" altText="Sport" title="Sport" />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Section imageSrc="/courses.jpeg" altText="Liste de courses" title="Liste de courses" />
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Section imageSrc="/todos.jpeg" altText="Todos sauvegardés" title="Todos sauvegardés" />
+          </Grid>
+        </Grid>
       </div>
     </>
   );
