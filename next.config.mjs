f483e0e9f895332af34cc1f360691f058815812b
Cédric LEVY <levycedric@hotmail.fr>
2024-10-27 19:40:56 +0100

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'Content-Security-Policy',
+              value: "default-src 'self'; img-src 'self' data:; script-src 'self' 'unsafe-inline' https://vercel.live; frame-src 'self'; connect-src 'self'; style-src 'self' 'unsafe-inline'",
+            },
+          ],
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
