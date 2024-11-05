57577efe0f1f9fa3ff190fe0db18aa9902302f64
Cédric LEVY <levycedric@hotmail.fr>
2024-11-05 20:32:40 +0100

    vercel

--- store/store.js
+++ store/store.js
@@ -25,7 +25,7 @@ export const store = configureStore({
   devTools: process.env.NODE_ENV !== 'production', // Outils de développement seulement en mode développement
 });
 
-// Créer un redux persistor
+// Créer un redcer persistor
 export const persistor = persistStore(store); // Exporter le persistor
 
 export default store;
